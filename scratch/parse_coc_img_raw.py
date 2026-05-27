becf126d3396b4bd0a59b8c5f6acbd6c3ce6748f
MarkJAWilson <mark@mwilson.on.ca>
2026-05-27 15:33:17 -0400

    Untrack scratch directory and add gitignore

--- scratch/parse_coc_img_raw.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-
-with open("scratch/coc_season.html", "r", encoding="utf-8") as f:
-    html = f.read()
-
-soup = BeautifulSoup(html, 'html.parser')
-for img in soup.find_all('img', class_='event-item__image')[:2]:
-    print("SRCSET:", img.get('srcset'))
-    print("SRC:", img.get('src'))
